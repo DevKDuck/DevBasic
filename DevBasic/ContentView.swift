3fcc0acf893fd67e83dc0de5c7d7c14366d186ee
DevKDuck <loading95@naver.com>
2023-06-07 15:37:43 +0900

    프로젝트 생성

--- DevBasic/ContentView.swift
+++ DevBasic/ContentView.swift
@@ -9,18 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView { // ✨
+            List {
+                CustomCell(cellNum: 2)
+                CustomCell(cellNum: 3)
+            }
+            .listStyle(GroupedListStyle())
+            .navigationBarTitle("List View")
         }
-        .padding()
     }
-}
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+}
+ 
+struct CustomCell: View {
+    var cellNum: Int
+ 
+    var body: some View {
+        HStack {
+            // ✨ 반드시 ! NavigationLink 로 감싸줘야함.
+            NavigationLink(destination: DetailView()) {
+                Image(systemName: "tortoise.fill")
+                Text("\(cellNum)")
+            }
+        }
     }
 }
+ 
+// Cell을 선택했을 때 이동될 View.
+struct DetailView: View {
+    var body: some View {
+        Image("펭수")
+    }
+}
+    
+    
+
