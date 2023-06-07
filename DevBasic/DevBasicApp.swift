f98243dc0f2af068b632758f4f6d5a9432ec2007
DevKDuck <loading95@naver.com>
2023-06-07 14:44:16 +0900

    Initial Commit

---
+++ DevBasic/DevBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevBasicApp.swift
+//  DevBasic
+//
+//  Created by duck on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct DevBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
